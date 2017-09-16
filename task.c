c446b12be488336d34af62cee8c3fed88ba24835
Екатерина <shalimova-ekaterina@yandex.ru>
2017-09-16 17:43:25 +0300

    First function on branch first

--- task.c
+++ task.c
@@ -1,61 +1,63 @@
 int h;
 
-void f(int a, int b)
-{
-  int x, y;
-  x = 3;
-  y = 1;
-  h = a;
-  if (h < y + a)
-    {
-      if (x > 7)
-        {
-          x = 2;
-        }
-      y = 8;
-    }
-  else
-    {
-      if (x < 4)
-        {
-          x = 4;
-        }
-      x = 2;
-    }
-}
+		void f(int a, int b)
+		{
+/*1  */	  int x, y;
+/*2  */	  x = 3;
+/*3  */	  y = 1;
+/*4  */	  h = a;
+/*5  */	  if (h < y + a)
+			{
+/*6  */		  if (x > 7)
+				{
+/*7  */			  x = 2;
+				}
+/*8  */		  y = 8;
+			}
+		  else
+			{
+/*9  */		  if (x < 4)
+				{
+/*10 */			  x = 4;
+				}
+/*11 */		  x = 2;
+			}
+/*12 */	}
 
-void g(int a, int b)
-{
-  int x, y;
-  x = 3;
-  y = 5;
-  h = 2;
-  if (x > 6)
-    {
-      if (h > x - a)
-        {
-          h = b;
-        }
-      x = 3;
-      h = x - b;
-    }
-  x = 1;
-  while (x < 7)
-    {
-      if (h > 0)
-        break;
-      if (y < 5)
-        {
-          h = x;
-        }
-      if (h < b - x)
-        {
-          if (x < 10)
-            {
-              x = 3;
-            }
-          y = 0;
-        }
-    }
-}
+//12 состояний
+
+		void g(int a, int b)
+		{
+		  int x, y;
+		  x = 3;
+		  y = 5;
+		  h = 2;
+		  if (x > 6)
+			{
+			  if (h > x - a)
+				{
+				  h = b;
+				}
+			  x = 3;
+			  h = x - b;
+			}
+		  x = 1;
+		  while (x < 7)
+			{
+			  if (h > 0)
+				break;
+			  if (y < 5)
+				{
+				  h = x;
+				}
+			  if (h < b - x)
+				{
+				  if (x < 10)
+					{
+					  x = 3;
+					}
+				  y = 0;
+				}
+			}
+		}
 
