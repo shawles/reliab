986c91d691fd174be5755325a43ba6240b067861
Екатерина <shalimova-ekaterina@yandex.ru>
2017-09-17 19:43:00 +0300

    Found some unreachable places

--- task.c
+++ task.c
@@ -2,51 +2,51 @@ int h;
 
 void f(int a, int b)
 {
-  int x, y; 		//h = {}, f_a = [], f_b = [], f_x = {}, f_y = {} 
-  x = 3;			//h = {}, f_a = [], f_b = [], f_x = {}, f_y = {}
-  y = 1;			//h = {}, f_a = [], f_b = [], f_x = {}, f_y = {}
-  h = a;			//h = {}, f_a = [], f_b = [], f_x = {}, f_y = {}
-  if (h < y + a)	//h = {}, f_a = [], f_b = [], f_x = {}, f_y = {}
+  int x, y; 		//h = {#, 2, g}, f_a = [-i, i], f_b = [-i, i], f_x = {#}, f_y = {#} 
+  x = 3;			//h = {#, 2, g}, f_a = [-i, i], f_b = [-i, i], f_x = {#}, f_y = {#}
+  y = 1;			//h = {#, 2, g}, f_a = [-i, i], f_b = [-i, i], f_x = {3}, f_y = {#}
+  h = a;			//h = {#, 2, g}, f_a = [-i, i], f_b = [-i, i], f_x = {3}, f_y = {1}
+  if (h < y + a)	//h = {f_a, 2, g}, f_a = [-i, i], f_b = [-i, i], f_x = {3}, f_y = {1}
     {
-      if (x > 7)	//h = {}, f_a = [], f_b = [], f_x = {}, f_y = {}
+      if (x > 7)	//h = {f_a, 2 if f_a > 1, g if g < 1 + f_a, g}, f_a = [], f_b = [], f_x = {3}, f_y = {1}
         {
-          x = 2;	//h = {}, f_a = [], f_b = [], f_x = {}, f_y = {}
+          x = 2;	//недостижимо
         }
-      y = 8;		//h = {}, f_a = [], f_b = [], f_x = {}, f_y = {}
+      y = 8;		//h = {f_a, 2 if f_a > 1, g in g < 1 + f_a, g}, f_a = [], f_b = [], f_x = {3}, f_y = {1}
     }
-  else 				//h = {}, f_a = [], f_b = [], f_x = {}, f_y = {}
+  else 				//h = {2 if f_a <= 1, g if g >= 1 + f_a, g}, f_a = [], f_b = [], f_x = {3}, f_y = {1}
     {
-      if (x < 4)	//h = {}, f_a = [], f_b = [], f_x = {}, f_y = {}
+      if (x < 4)	//h = {2 if f_a <= 1, g if g >= 1 + f_a, g}, f_a = [], f_b = [], f_x = {3}, f_y = {1}
         {
-          x = 4;	//h = {}, f_a = [], f_b = [], f_x = {}, f_y = {}
+          x = 4;	//h = {2 if f_a <= 1, g if g >= 1 + f_a, g}, f_a = [], f_b = [], f_x = {3}, f_y = {1}
         }
-      x = 2;		//h = {}, f_a = [], f_b = [], f_x = {}, f_y = {}
+      x = 2;		//h = {2 if f_a <= 1, g if g >= 1 + f_a, g}, f_a = [], f_b = [], f_x = {4}, f_y = {1}
     }
-}					//h = {}, f_a = [], f_b = [], f_x = {}, f_y = {}
+}					//h = {2, f_a, g}, f_a = [], f_b = [], f_x = {3, 4}, f_y = {8, 1}
 
 void g(int a, int b)
 {
-  int x, y;			//h = {}, g_a = [], g_b = [], g_x = {}, g_y = {}
-  x = 3;			//h = {}, g_a = [], g_b = [], g_x = {}, g_y = {}
-  y = 5;			//h = {}, g_a = [], g_b = [], g_x = {}, g_y = {}
-  h = 2;			//h = {}, g_a = [], g_b = [], g_x = {}, g_y = {}
-  if (x > 6)		//h = {}, g_a = [], g_b = [], g_x = {}, g_y = {}
+  int x, y;			//h = {#, f}, g_a = [-int, int], g_b = [-int, int], g_x = {#}, g_y = {#}
+  x = 3;			//h = {#, f}, g_a = [-int, int], g_b = [-int, int], g_x = {#}, g_y = {#}
+  y = 5;			//h = {#, f}, g_a = [-int, int], g_b = [-int, int], g_x = {3}, g_y = {#}
+  h = 2;			//h = {#, f}, g_a = [-int, int], g_b = [-int, int], g_x = {3}, g_y = {5}
+  if (x > 6)		//h = {2, f}, g_a = [-int, int], g_b = [-int, int], g_x = {3}, g_y = {5}
     {
-      if (h > x - a)//h = {}, g_a = [], g_b = [], g_x = {}, g_y = {}
+      if (h > x - a)//недостижимо
         {
-          h = b;	//h = {}, g_a = [], g_b = [], g_x = {}, g_y = {}
+          h = b;	//недостижимо
         }
-      x = 3;		//h = {}, g_a = [], g_b = [], g_x = {}, g_y = {}
-      h = x - b;	//h = {}, g_a = [], g_b = [], g_x = {}, g_y = {}
+      x = 3;		//недостижимо
+      h = x - b;	//недостижимо
     }
-  x = 1;			//h = {}, g_a = [], g_b = [], g_x = {}, g_y = {}
-  while (x < 7)		//h = {}, g_a = [], g_b = [], g_x = {}, g_y = {}
+  x = 1;			//h = {2, f}, g_a = [-int, int], g_b = [-int, int], g_x = {3}, g_y = {5}
+  while (x < 7)		//?  h = {2, f}, g_a = [-int, int], g_b = [-int, int], g_x = {1}, g_y = {5}
     {
-      if (h > 0)	//h = {}, g_a = [], g_b = [], g_x = {}, g_y = {}
-        break;		//h = {}, g_a = [], g_b = [], g_x = {}, g_y = {}
-      if (y < 5)	//h = {}, g_a = [], g_b = [], g_x = {}, g_y = {}
+      if (h > 0)	//?  h = {2, f}, g_a = [-int, int], g_b = [-int, int], g_x = {1}, g_y = {5}
+        break;		//?  h = {2, f > 0}, g_a = [-int, int], g_b = [-int, int], g_x = {1}, g_y = {5}
+      if (y < 5)	//?  h = {f < 0}, g_a = [-int, int], g_b = [-int, int], g_x = {1}, g_y = {5}
         {
-          h = x;	//h = {}, g_a = [], g_b = [], g_x = {}, g_y = {}
+          h = x;	//пока недостижимо в первом цикле h = {}, g_a = [], g_b = [], g_x = {}, g_y = {}
         }
       if (h < b - x)//h = {}, g_a = [], g_b = [], g_x = {}, g_y = {}
         {
