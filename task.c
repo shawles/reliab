177242ccb1c936b4b82683e4254d2ae8700649db
Екатерина <shalimova-ekaterina@yandex.ru>
2017-09-17 15:45:41 +0300

    Added sample lines

--- task.c
+++ task.c
@@ -2,60 +2,60 @@ int h;
 
 void f(int a, int b)
 {
-  int x, y;
-  x = 3;
-  y = 1;
-  h = a;
-  if (h < y + a)
+  int x, y; 		//h = {}, f_a = [], f_b = [], f_x = {}, f_y = {} 
+  x = 3;			//h = {}, f_a = [], f_b = [], f_x = {}, f_y = {}
+  y = 1;			//h = {}, f_a = [], f_b = [], f_x = {}, f_y = {}
+  h = a;			//h = {}, f_a = [], f_b = [], f_x = {}, f_y = {}
+  if (h < y + a)	//h = {}, f_a = [], f_b = [], f_x = {}, f_y = {}
     {
-      if (x > 7)
+      if (x > 7)	//h = {}, f_a = [], f_b = [], f_x = {}, f_y = {}
         {
-          x = 2;
+          x = 2;	//h = {}, f_a = [], f_b = [], f_x = {}, f_y = {}
         }
-      y = 8;
+      y = 8;		//h = {}, f_a = [], f_b = [], f_x = {}, f_y = {}
     }
-  else
+  else 				//h = {}, f_a = [], f_b = [], f_x = {}, f_y = {}
     {
-      if (x < 4)
+      if (x < 4)	//h = {}, f_a = [], f_b = [], f_x = {}, f_y = {}
         {
-          x = 4;
+          x = 4;	//h = {}, f_a = [], f_b = [], f_x = {}, f_y = {}
         }
-      x = 2;
+      x = 2;		//h = {}, f_a = [], f_b = [], f_x = {}, f_y = {}
     }
-}
+}					//h = {}, f_a = [], f_b = [], f_x = {}, f_y = {}
 
 void g(int a, int b)
 {
-  int x, y;
-  x = 3;
-  y = 5;
-  h = 2;
-  if (x > 6)
+  int x, y;			//h = {}, g_a = [], g_b = [], g_x = {}, g_y = {}
+  x = 3;			//h = {}, g_a = [], g_b = [], g_x = {}, g_y = {}
+  y = 5;			//h = {}, g_a = [], g_b = [], g_x = {}, g_y = {}
+  h = 2;			//h = {}, g_a = [], g_b = [], g_x = {}, g_y = {}
+  if (x > 6)		//h = {}, g_a = [], g_b = [], g_x = {}, g_y = {}
     {
-      if (h > x - a)
+      if (h > x - a)//h = {}, g_a = [], g_b = [], g_x = {}, g_y = {}
         {
-          h = b;
+          h = b;	//h = {}, g_a = [], g_b = [], g_x = {}, g_y = {}
         }
-      x = 3;
-      h = x - b;
+      x = 3;		//h = {}, g_a = [], g_b = [], g_x = {}, g_y = {}
+      h = x - b;	//h = {}, g_a = [], g_b = [], g_x = {}, g_y = {}
     }
-  x = 1;
-  while (x < 7)
+  x = 1;			//h = {}, g_a = [], g_b = [], g_x = {}, g_y = {}
+  while (x < 7)		//h = {}, g_a = [], g_b = [], g_x = {}, g_y = {}
     {
-      if (h > 0)
-        break;
-      if (y < 5)
+      if (h > 0)	//h = {}, g_a = [], g_b = [], g_x = {}, g_y = {}
+        break;		//h = {}, g_a = [], g_b = [], g_x = {}, g_y = {}
+      if (y < 5)	//h = {}, g_a = [], g_b = [], g_x = {}, g_y = {}
         {
-          h = x;
+          h = x;	//h = {}, g_a = [], g_b = [], g_x = {}, g_y = {}
         }
-      if (h < b - x)
+      if (h < b - x)//h = {}, g_a = [], g_b = [], g_x = {}, g_y = {}
         {
-          if (x < 10)
+          if (x < 10)//h = {}, g_a = [], g_b = [], g_x = {}, g_y = {}
             {
-              x = 3;
+              x = 3;//h = {}, g_a = [], g_b = [], g_x = {}, g_y = {}
             }
-          y = 0;
+          y = 0;	//h = {}, g_a = [], g_b = [], g_x = {}, g_y = {}
         }
     }
-}
+}					//h = {}, g_a = [], g_b = [], g_x = {}, g_y = {}
 
