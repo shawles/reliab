4e291a874c9ad65415b510c431612c0755b48fee
Екатерина <shalimova-ekaterina@yandex.ru>
2017-09-17 20:16:53 +0300

    Found h = g_x = 3

--- task.c
+++ task.c
@@ -20,29 +20,31 @@ void f(int a, int b)
     }
 }					//h = {2, f_a, g}, f_a = [], f_b = [], f_x = {3, 4}, f_y = {8, 1}
 
+//f может сменить h только на f_a, причём это f_a не меняется вообще
+
 void g(int a, int b)
 {
-  int x, y;			//h = {#, f}, g_a = [-int, int], g_b = [-int, int], g_x = {#}, g_y = {#}
-  x = 3;			//h = {#, f}, g_a = [-int, int], g_b = [-int, int], g_x = {#}, g_y = {#}
-  y = 5;			//h = {#, f}, g_a = [-int, int], g_b = [-int, int], g_x = {3}, g_y = {#}
-  h = 2;			//h = {#, f}, g_a = [-int, int], g_b = [-int, int], g_x = {3}, g_y = {5}
-  x = 1;			//h = {2, f}, g_a = [-int, int], g_b = [-int, int], g_x = {3}, g_y = {5}
-  while (x < 7)		//?  h = {2, f}, g_a = [-int, int], g_b = [-int, int], g_x = {1}, g_y = {5}
+  int x, y;			//h = {#, f_a}, g_a = [-int, int], g_b = [-int, int], g_x = {#}, g_y = {#}
+  x = 3;			//h = {#, f_a}, g_a = [-int, int], g_b = [-int, int], g_x = {#}, g_y = {#}
+  y = 5;			//h = {#, f_a}, g_a = [-int, int], g_b = [-int, int], g_x = {3}, g_y = {#}
+  h = 2;			//h = {#, f_a}, g_a = [-int, int], g_b = [-int, int], g_x = {3}, g_y = {5}
+  x = 1;			//h = {2, f_a}, g_a = [-int, int], g_b = [-int, int], g_x = {3}, g_y = {5}
+  while (x < 7)		//h = {2, f_a, 3 if 6 < g_b}, g_a = [-int, int], g_b = [-int, int], g_x = {1, 3}, g_y = {5, 0}
     {
-      if (h > 0)	//?  h = {2, f}, g_a = [-int, int], g_b = [-int, int], g_x = {1}, g_y = {5}
-        break;		//?  h = {2, f > 0}, g_a = [-int, int], g_b = [-int, int], g_x = {1}, g_y = {5}
-      if (y < 5)	//?  h = {f < 0}, g_a = [-int, int], g_b = [-int, int], g_x = {1}, g_y = {5}
+      if (h > 0)	//h = {2, f_a, 3 if 6 < g_b}, g_a = [-int, int], g_b = [-int, int], g_x = {1, 3}, g_y = {5, 0}
+        break;		//h = {2, f_a, 3 if 6 < g_b}, g_a = [-int, int], g_b = [-int, int], g_x = {1, 3}, g_y = {5, 0}
+      if (y < 5)	//h = {f_a}, g_a = [-int, int], g_b = [-int, int], g_x = {1, 3}, g_y = {5, 0}
         {
-          h = x;	//пока недостижимо в первом цикле h = {}, g_a = [], g_b = [], g_x = {}, g_y = {}
+          h = x;	//h = {f_a}, g_a = [-i, i], g_b = [-i, i], g_x = {3}, g_y = {0}
         }
-      if (h < b - x)//h = {}, g_a = [], g_b = [], g_x = {}, g_y = {}
+      if (h < b - x)//h = {f_a, 3}, g_a = [-i, i], g_b = [-i, i], g_x = {1, 3}, g_y = {5, 0}
         {
-          if (x < 10)//h = {}, g_a = [], g_b = [], g_x = {}, g_y = {}
+          if (x < 10)//h = {f_a, 3 if 6 < g_b}, g_a = [-i, i], g_b = [-i, i...], g_x = {1, 3}, g_y = {5, 0}
             {
-              x = 3;//h = {}, g_a = [], g_b = [], g_x = {}, g_y = {}
+              x = 3;//h = {f_a, 3 if 6 < g_b}, g_a = [-i, i], g_b = [-i, i...], g_x = {1, 3}, g_y = {5, 0}
             }
-          y = 0;	//h = {}, g_a = [], g_b = [], g_x = {}, g_y = {}
+          y = 0;	//h = {f_a, 3 if 6 < g_b}, g_a = [-i, i], g_b = [-i, i...], g_x = {3}, g_y = {5, 0}
         }
     }
 }					//h = {}, g_a = [], g_b = [], g_x = {}, g_y = {}
-
+//g может сменить h только на f_х = 3
